ec5b3f5979f549ec33960c8e1993fc4d7c5102c8
Hing <32828455+gddcx@users.noreply.github.com>
2023-08-21 02:02:40 +0800

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -1,12 +1,12 @@
 # C+T -> S/K
 # For EMD-S
-python evaluate.py --model EMD_things_S.pth --dataset sintel --iters8 18 --iters4 0 --model_type S
-python evaluate.py --model EMD_things_S.pth --dataset kitti --iters8 18 --iters4 0 --model_type S
+python evaluate.py --model weights/EMD_things_S.pth --dataset sintel --iters8 18 --iters4 0 --model_type S
+python evaluate.py --model weights/EMD_things_S.pth --dataset kitti --iters8 18 --iters4 0 --model_type S
 
 # For EMD-M
-python evaluate.py --model EMD_things_M.pth --dataset sintel --iters8 18 --iters4 6 --model_type M
-python evaluate.py --model EMD_things_M.pth --dataset kitti --iters8 18 --iters4 6 --model_type M
+python evaluate.py --model weights/EMD_things_M.pth --dataset sintel --iters8 18 --iters4 6 --model_type M
+python evaluate.py --model weights/EMD_things_M.pth --dataset kitti --iters8 18 --iters4 6 --model_type M
 
 # For EMD-M
-python evaluate.py --model EMD_things_L.pth --dataset sintel --iters8 18 --iters4 6 --model_type L
-python evaluate.py --model EMD_things_L.pth --dataset kitti --iters8 18 --iters4 6 --model_type L
+python evaluate.py --model weights/EMD_things_L.pth --dataset sintel --iters8 18 --iters4 6 --model_type L
+python evaluate.py --model weights/EMD_things_L.pth --dataset kitti --iters8 18 --iters4 6 --model_type L
